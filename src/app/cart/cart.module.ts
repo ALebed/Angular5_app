b0beff9053c5091fbfacd53773f62f0482ad7281
Anna Lebed <Anna_Lebed@epam.com>
2018-08-27 16:21:38 +0300

    Task2. Remove duplicates of services declaration

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CartComponent} from './list/cart.component';
-import {CartService} from './cart.service';
 import {ItemComponent} from './item/item.component';
 import {HoverStyleDirective} from './item/hover-style.directive';
 
@@ -14,7 +13,6 @@ import {HoverStyleDirective} from './item/hover-style.directive';
         ItemComponent,
         HoverStyleDirective
     ],
-    providers: [CartService],
     exports: [CartComponent]
 })
 export class CartModule {}
